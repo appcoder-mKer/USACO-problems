43b81ba6b2c3325ccfc4bbd64fe6257862932a64
Nakul_sethi <82829022+appcoder-mKer@users.noreply.github.com>
2024-11-16 19:19:02 -0500

    Add template file

---
+++ className.java
@@ -0,0 +1,13 @@
+import java.util.*;  
+import java.io.*;  
+public class className {  
+    public static void main(String[] args) throws IOException{  
+        File inputFile = new File("program.in");
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("programName.out")));
+        Scanner fileScanner = new Scanner(inputFile);
+
+
+
+            out.close();  
+        }  
+}  
